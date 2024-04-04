f6b69a2e1c9785b38cea36dc89ee86a1101da263
Son Vo <vnsonvo@gmail.com>
2024-04-04 16:47:34 +0700

    connect to postgres db using gorm

--- main.go
+++ main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func initProgram() {
+var DB *gorm.DB
+
+func connectToDb() {
+	dsn := os.Getenv("DB")
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to DB")
+	}
+}
+
+func init() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	connectToDb()
 }
 
 func main() {
-	initProgram()
+	r := gin.Default()
+	port := os.Getenv("PORT")
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":" + port)
 }
